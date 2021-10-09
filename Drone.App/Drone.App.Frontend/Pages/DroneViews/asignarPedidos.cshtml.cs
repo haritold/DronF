e70215328976dc2f7a1903c4895dd56b21a9ac65
Juan Agudelo <juan.carlos.agudeloxp@gmail.com>
2021-10-09 15:57:35 -0500

    sugrencia profesor 9-10-2021

--- Drone.App/Drone.App.Frontend/Pages/DroneViews/asignarPedidos.cshtml.cs
+++ Drone.App/Drone.App.Frontend/Pages/DroneViews/asignarPedidos.cshtml.cs
@@ -5,12 +5,23 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
+using Drone.App.Dominio;
+using Drone.App.Persistencia;
+
+
+
 namespace Drone.App.Frontend.Pages
 {
     public class asignarPedidosModel : PageModel
     {
+        private static IRepositorioPaciente _repoPaciente= new RepositorioPaciente(new Persistencia.AppContext());
+
+        public List<Encargado> ListaEncargado{get;set;}
+
         public void OnGet()
         {
+            ListaEncargado = _repoEncargado.GetEncargado().ToList();
+            //ListarPacientesCorazon();
         }
     }
 }
--- Drone.App/Drone.App.Frontend/Pages/DroneViews/pedido.cshtml.cs
+++ Drone.App/Drone.App.Frontend/Pages/DroneViews/pedido.cshtml.cs
@@ -5,6 +5,8 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
+
+
 namespace Drone.App.Frontend.Pages
 {
     public class pedidoModel : PageModel
