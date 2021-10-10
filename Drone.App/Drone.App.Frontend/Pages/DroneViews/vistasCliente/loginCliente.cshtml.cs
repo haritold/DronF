1bef4b884a74ffd55da03f3583ec16c053fd6597
Garygreenman <79773051+Garygreenman@users.noreply.github.com>
2021-10-10 01:06:17 -0500

    Metodo add cliente a la base de datos desde el front

--- Drone.App/Drone.App.Frontend/Pages/DroneViews/vistasCliente/loginCliente.cshtml.cs
+++ Drone.App/Drone.App.Frontend/Pages/DroneViews/vistasCliente/loginCliente.cshtml.cs
@@ -1,3 +1,7 @@
+using System.Net.Security;
+using System.Net.Http;
+using System.Net.WebSockets;
+using System.IO;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -5,13 +9,65 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
+using Drone.App.Dominio;
+using Drone.App.Persistencia;
+
 namespace Drone.App.Frontend.Pages
 {
     public class loginClienteModel : PageModel
     {
-        public void OnGet()
+        private readonly IRepositorioCliente repositorioCliente;
+
+        [BindProperty]
+
+        public Cliente Cliente {get; set;}
+
+        public loginClienteModel(IRepositorioCliente repositorioCliente)
         {
+            this.repositorioCliente=repositorioCliente;
+        }
+
+        public IActionResult OnGet(int? clienteId)
+        {
+            if(clienteId.HasValue)
+            {
+                Cliente = repositorioCliente.GetClientePorId(clienteId.Value);
+            }
+            else
+            {
+                Cliente = new Cliente();
+            }
+
+            if(Cliente == null)
+            {
+                return RedirectToPage("./NotFound");
+            }
+            else
+            {
+                return Page();
+            }
+
             
         }
+
+        public IActionResult OnPost()
+        {
+            if(!ModelState.IsValid)
+            {
+                return Page();
+            }
+            if(Cliente.Id>0)
+            {
+                Cliente = repositorioCliente.Update(Cliente);
+            }
+            else
+            {
+                repositorioCliente.AddCliente(Cliente);
+            }
+            return Page();
+
+        }
+
+
     }
 }
